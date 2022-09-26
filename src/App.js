8d9a5d53daf10dc3adbd278d432eb9437a155ab4
abhi1518 <60543134+abhi1518@users.noreply.github.com>
2022-09-26 13:23:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './Components/Homepage/Homepage';
+import Navb from './Components/Navb/Navb';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+   <>
+    <Navb />
+   <Homepage/>
+   <Footer />
+   </>
+  );
+}
+
+export default App;
